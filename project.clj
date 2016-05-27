7d81bacd2838de8f6a5568660cca1535a014c044
Bill Tozier <bill@vagueinnovation.com>
2016-05-26 20:33:29 -0400

    initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject winkler-spider "0.0.1-SNAPSHOT"
+  :description "Some ants and spiders on an abstract graph."
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
---
+++ test/cube/core_test.clj
@@ -0,0 +1,5 @@
+(ns cube.core-test
+  (:use midje.sweet)
+  (:use cube.core))
+
+(fact (+ 2 2) => 4)
